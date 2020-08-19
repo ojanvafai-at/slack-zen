cacefce216e278b154513fc0f07f29a34c4acae8
Ojan Vafai <ojan@airtable.com>
2020-08-18 19:42:12 -0700

    Hide sidebar on a delay when blurring window and hide empty section names.

--- content-script.js
+++ content-script.js
@@ -1,4 +1,10 @@
 const className = "hide-sidebar";
+// Don't hide the sidebar immediately on blur if the user goes away and comes
+// back quickly.
+const hideDelayMs = 1000 * 60 * 15;
+let delayTimeout;
+let windowFocused;
+let showEmptySections = false;
 
 function show(shouldShow) {
   const classList = document.documentElement.classList;
@@ -10,12 +16,34 @@ function show(shouldShow) {
 }
 show(false);
 
-function toggle() {
+function hideAfterDelayIfStillVisible() {
+  if (delayTimeout) {
+    clearTimeout(delayTimeout);
+  }
+  delayTimeout = setTimeout(() => {
+    if (!windowFocused || document.visibilityState !== "visible") {
+      show(false);
+    }
+  }, hideDelayMs);
+}
+
+function toggleSidebar() {
   show(document.documentElement.classList.contains(className));
+}
+
+function toggleShowEmptySections() {
+  showEmptySections = !showEmptySections;
+  updateEmptySectionDisplay();
 }
 
 function favicon() {
   return document.querySelector('link[rel*="icon"]');
+}
+
+function channelSidebarList() {
+  return document.querySelector(
+    ".p-channel_sidebar__list .c-virtual_list__scroll_container"
+  );
 }
 
 function debadgeFavicon() {
@@ -27,21 +55,112 @@ function isFavicon(node) {
   return node.tagName === "LINK" && node.getAttribute("rel").includes("icon");
 }
 
-window.addEventListener("visibilitychange", () => show(false));
-window.addEventListener("blur", () => show(false));
+function isChannelSidebarSection(element) {
+  return element.id.startsWith("sectionHeading-");
+}
 
-window.addEventListener("load", () => {
-  const observer = new MutationObserver((mutations) => {
+window.addEventListener("visibilitychange", hideAfterDelayIfStillVisible);
+window.addEventListener("blur", () => {
+  windowFocused = false;
+  hideAfterDelayIfStillVisible();
+});
+window.addEventListener("focus", () => (windowFocused = true));
+
+function observe(getObserved, callback) {
+  if (!getObserved()) {
+    setTimeout(() => observe(getObserved, callback), 1000);
+    return;
+  }
+  const observer = new MutationObserver(callback);
+  observer.observe(getObserved().parentNode, {
+    childList: true,
+    subtree: true,
+  });
+}
+
+function containsSelector(element, selector) {
+  return !!element.querySelector(selector);
+}
+
+function isSectionCollapsed(section) {
+  return containsSelector(
+    section,
+    ".p-channel_sidebar__section_heading--collapsed"
+  );
+}
+
+function hasPlusButton(section) {
+  return containsSelector(section, ".p-channel_sidebar__section_heading_plus");
+}
+
+function labelElement(section) {
+  return section.querySelector(
+    ".p-channel_sidebar__section_heading_label_overflow"
+  );
+}
+
+function hasSectionName(section, name) {
+  const sectionLabel = labelElement(section);
+  return sectionLabel && sectionLabel.textContent === name;
+}
+
+function updateEmptySectionDisplay() {
+  const sidebarItems = channelSidebarList().children;
+  for (let i = 0; i < sidebarItems.length; i++) {
+    const item = sidebarItems[i];
+    if (!isChannelSidebarSection(item)) {
+      continue;
+    }
+    // The next sibling after a section with no unreads is area metadata, and
+    // the one after that is either a channel or a section. If it's a section
+    // then this section is empty.
+    let shouldHide;
+    if (showEmptySections || hasPlusButton(item)) {
+      shouldHide = false;
+    } else if (hasSectionName(item, "Hidden")) {
+      shouldHide = true;
+    } else {
+      shouldHide =
+        i >= sidebarItems.length - 2 ||
+        isChannelSidebarSection(sidebarItems[i + 2]);
+    }
+    const sectionLabel = labelElement(item);
+    if (sectionLabel) {
+      sectionLabel.style.visibility = shouldHide ? "hidden" : "";
+    }
+  }
+}
+
+function observeChanngelSidebarListChanges() {
+  observe(channelSidebarList, (mutations) => updateEmptySectionDisplay());
+  updateEmptySectionDisplay();
+}
+
+function observeFaviconChanges() {
+  observe(favicon, (mutations) => {
     if (mutations.some((x) => [...x.removedNodes].some((y) => isFavicon(y)))) {
       debadgeFavicon();
     }
   });
-  observer.observe(favicon().parentNode, { childList: true });
   debadgeFavicon();
+}
 
-  let toggleButton = document.createElement("button");
-  toggleButton.classList.add("hide-sidebar-toggle-button");
-  toggleButton.textContent = "Toggle sidebar";
-  toggleButton.addEventListener("click", toggle);
-  document.querySelector(".p-top_nav__sidebar").before(toggleButton);
+function createButton(text, callback) {
+  let button = document.createElement("button");
+  button.classList.add("hide-sidebar-toggle-button");
+  button.textContent = text;
+  button.addEventListener("click", callback);
+  return button;
+}
+
+window.addEventListener("load", () => {
+  observeFaviconChanges();
+  observeChanngelSidebarListChanges();
+
+  document
+    .querySelector(".p-top_nav__sidebar")
+    .before(
+      createButton("Toggle sidebar", toggleSidebar),
+      createButton("Toggle show empty sections", toggleShowEmptySections)
+    );
 });
